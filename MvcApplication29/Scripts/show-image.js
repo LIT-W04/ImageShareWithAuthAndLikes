f4d7c37c80ddd7449fdd431e515e1fc871491800
Avrumi Friedman <info@lakewoodprogramming.com>
2017-07-13 21:35:31 -0400

    Initial commit

---
+++ MvcApplication29/Scripts/show-image.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    var id = $(".row").data('image-id');
+    setInterval(function () {
+        $.get('/home/getcount', { id: id }, function (result) {
+            $("#viewCount").text(result.viewCount);
+            $("#likesCount").text(result.likes);
+        });
+        //        $.get('/home/getlikes', { imageId: id }, function(result) {
+        //            $("#likesCount").text(result.likes);
+        //        });
+    }, 500);
+
+    $("#like-button").on('click', function () {
+        $.post("/home/likeimage", { imageId: id }, function () {
+            $("#like-button").html('<span class="glyphicon glyphicon-thumbs-up"></span> Liked!');
+            $("#like-button").prop("disabled", true);
+        });
+    });
+});
